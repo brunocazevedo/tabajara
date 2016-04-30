799bbe92a6e317e01bfc263625303e0cf50c21f6
brunocazevedo <brunosantos0220@gmail.com>
2016-04-30 18:17:33 -0300

    Iniciando o projeto

---
+++ js/controllers.js
@@ -0,0 +1,22 @@
+angular.module('app.controllers', [])
+  
+.controller('homeCtrl', function($scope) {
+
+})
+      
+.controller('empresaCtrl', function($scope) {
+
+})
+   
+.controller('produtoCtrl', function($scope) {
+
+})
+   
+.controller('serviOCtrl', function($scope) {
+
+})
+   
+.controller('contatoCtrl', function($scope) {
+
+})
+ 
